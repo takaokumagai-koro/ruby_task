03bc3d516913a0b1fe9a9b597386a68cdd0521f0
kumagai <kumagai.takao.7@gmail.com>
2020-01-24 07:02:41 +0900

    ruby_task_1_7

--- task.rb
+++ task.rb
@@ -2,9 +2,11 @@
 # 回答の出力を確認される際は，「ruby main.rb」をターミナルから実行して下さい。
 
 def q1
-  names = ["田中", "佐藤", "佐々木", "高橋"]
+  names = ["田中", "佐藤", "佐々木", "高橋", "斎藤"]
 
   # 以下に回答を記載
+
+  puts names[4]
 
 end
 
@@ -14,19 +16,24 @@ def q2
 
   # 以下に回答を記載
 
+  array = [array1, array2]
+  puts array
+
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  puts numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  sports.delete(nil)
+  puts sports
 
 end
 
@@ -35,6 +42,12 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+ 
+  if array1.size == 0
+    puts true
+  else
+    puts false
+  end
 
 end
 
@@ -42,6 +55,7 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  p numbers1.map {|num| num * 10}
 
 end
 
@@ -49,6 +63,9 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+
+
+  p array.map(&:to_i)
 
 end
 
